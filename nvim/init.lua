6c0190381b7fca4f21f5ca96027fd3b99edef7aa
LunamNauta <ayden.kranz@gmail.com>
2024-10-16 12:15:37 -0400

    Upload from 'UploadConfig' (16-Oct-24 12:15:37 PM)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,2 @@
 require("wvim"):init()
+--Test
--- nvim/lua/wvim/utils/git.lua
+++ nvim/lua/wvim/utils/git.lua
@@ -5,7 +5,7 @@ function wvim.git.UploadConfig()
     local git_dir = vim.fn.stdpath("config")
     local git_command = "!git -C " .. git_dir
     local commit_msg = "Upload from 'UploadConfig' (" .. vim.fn.strftime("%c") .. ")"
-    vim.cmd(git_command .. " add *")
+    vim.cmd(git_command .. " add .")
     vim.cmd(git_command .. " commit -m \"" .. commit_msg .. "\"")
     vim.cmd(git_command .. " push origin main")
 end

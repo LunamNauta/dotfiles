660ca5955f828df6c81516184830df4d99dcd0a4
LunamNauta <ayden.kranz@gmail.com>
2024-10-16 12:13:22 -0400

    Upload from 'UploadConfig' (16-Oct-24 12:13:22 PM)

---
+++ firefox/user-overrides.js
@@ -0,0 +1,15 @@
+user_pref("_user.js.parrot", "user-overrides.js Error: OMG, THE HUMANITY!!!");
+
+user_pref("browser.startup.page", 1);
+user_pref("browser.startup.homepage", "about:home");
+user_pref("browser.newtabpage.enabled", true);
+
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.showWeather", false);
+user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
+user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+
+user_pref("extensions.pocket.enabled", false);
+
+user_pref("_user.js.parrot", "SUCCESS: No no he's not dead, he's, he's restin'!");

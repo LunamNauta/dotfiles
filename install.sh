3bf6c03c597d0c5e61f5ea003772eed5db625258
Ayden Kranz <ayden.kranz@gmail.com>
2024-11-17 02:29:25 -0500

    Fully switched to sway over hypr. Added pcmanfm-qt as file manager. Added qt themeing

--- install.sh
+++ install.sh
@@ -12,15 +12,28 @@ set-config-link(){
     fi
 }
 
+# Basic Config Folders
 set-config-link "$SCRIPTPATH/waybar" "$HOME/.config/waybar"
+set-config-link "$SCRIPTPATH/fuzzel" "$HOME/.config/fuzzel"
 set-config-link "$SCRIPTPATH/foot" "$HOME/.config/foot"
 set-config-link "$SCRIPTPATH/nvim" "$HOME/.config/nvim"
 set-config-link "$SCRIPTPATH/hypr" "$HOME/.config/hypr"
 set-config-link "$SCRIPTPATH/sway" "$HOME/.config/sway"
 set-config-link "$SCRIPTPATH/tmux" "$HOME/.config/tmux"
+
+# Basic Config Files
 set-config-link "$SCRIPTPATH/.tmux.conf" "$HOME/.tmux.conf"
-set-config-link "$SCRIPTPATH/fuzzel" "$HOME/.config/fuzzel"
 set-config-link "$SCRIPTPATH/.bash_profile" "$HOME/.bash_profile"
 set-config-link "$SCRIPTPATH/.bashrc" "$HOME/.bashrc"
+
+# Special cases (QT)
+if ! [ -d "$HOME/.config/qt5ct" ]; then
+    mkdir "$HOME/.config/qt5ct"
+fi
+if ! [ -d "$HOME/.config/qt6ct" ]; then
+    mkdir "$HOME/.config/qt6ct"
+fi
+set-config-link "$SCRIPTPATH/qt5ct/colors" "$HOME/.config/qt5ct/colors"
+set-config-link "$SCRIPTPATH/qt6ct/colors" "$HOME/.config/qt6ct/colors"
 
 unset -f set-config-link
--- wofi/run-wofi.sh
+++
@@ -1,6 +0,0 @@
-XDG_DATA_DIRS="$HOME/.config/wofi/share"
-XDG_DATA_HOME="/dev/null"
-XDG_CACHE_HOME="/dev/null"
-/usr/bin/wofi --show=drun --cache="$HOME/.config/wofi/wofi-drun-cache"
-
-

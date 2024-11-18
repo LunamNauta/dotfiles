1a4d0eb96a01075a8425b1701e2595f3d17b4638
Ayden Kranz <ayden.kranz@gmail.com>
2024-11-17 21:48:53 -0500

    Got themeing to work in LibreOffice. Moved from pcmanfm to thunar

--- install.sh
+++ install.sh
@@ -26,7 +26,22 @@ set-config-link "$SCRIPTPATH/.tmux.conf" "$HOME/.tmux.conf"
 set-config-link "$SCRIPTPATH/.bash_profile" "$HOME/.bash_profile"
 set-config-link "$SCRIPTPATH/.bashrc" "$HOME/.bashrc"
 
-# Special cases (QT)
+# Special Case (LibreOffice)
+if ! [ -d "$HOME/.config/libreoffice" ]; then
+    mkdir "$HOME/.config/libreoffice" 
+fi
+if ! [ -d "$HOME/.config/libreoffice/4" ]; then
+    mkdir "$HOME/.config/libreoffice/4"
+fi
+if ! [ -d "$HOME/.config/libreoffice/4/user" ]; then
+    mkdir "$HOME/.config/libreoffice/4/user"
+fi
+if ! [ -d "$HOME/.config/libreoffice/4/user/config" ]; then
+    mkdir "$HOME/.config/libreoffice/4/user/config"
+fi
+set-config-link "$SCRIPTPATH/libreoffice/catppuccin-mocha-lavender.soc" "$HOME/.config/libreoffice/4/user/config/catppuccin-mocha-lavender.soc"
+
+# Special Case (QT)
 if ! [ -d "$HOME/.config/qt5ct" ]; then
     mkdir "$HOME/.config/qt5ct"
 fi
@@ -35,5 +50,15 @@ if ! [ -d "$HOME/.config/qt6ct" ]; then
 fi
 set-config-link "$SCRIPTPATH/qt5ct/colors" "$HOME/.config/qt5ct/colors"
 set-config-link "$SCRIPTPATH/qt6ct/colors" "$HOME/.config/qt6ct/colors"
+
+# Special Case (GTK)
+if ! [ -d "$HOME/.local" ]; then
+    mkdir "$HOME/.local"
+fi
+if ! [ -d "$HOME/.local/share" ]; then
+    mkdir "$HOME/.local/share"
+fi
+set-config-link "$SCRIPTPATH/gtk/themes" "$HOME/.local/share/themes"
+dconf load / < "$SCRIPTPATH/dconf.ini"
 
 unset -f set-config-link

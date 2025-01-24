254a4c5210ca6689888d5171f7179036c3932d59
Ayden Kranz <ayden.kranz@gmail.com>
2025-01-24 18:37:59 -0500

    Updated the neovim config... Again

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -23,7 +23,6 @@ return{
             telescope.setup(opts)
             telescope.load_extension('file_browser')
             telescope.load_extension('fzf')
-            vim.keymap.set("n", "<LEADER>ff", "<CMD>Telescope file_browser<CR>")
         end
     },
     {

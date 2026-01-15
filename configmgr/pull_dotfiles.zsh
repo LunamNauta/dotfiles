883114f5c67f3fedfce2ec83e60e9e213bfe778f
Ayden Kranz <ayden.kranz@gmail.com>
2026-01-15 01:07:39 -0500

    Added starship nonsense

--- configmgr/pull_dotfiles.zsh
+++ configmgr/pull_dotfiles.zsh
@@ -3,17 +3,17 @@ source $SCRIPT_PATH/utils.zsh
 
 if ! [[ -d $HOME/.config ]]; then exit; fi
 
-pull-config-copy $SCRIPT_PATH/../bluetuith          $HOME/.config/bluetuith    bluetuith
-pull-config-copy $SCRIPT_PATH/../fuzzel             $HOME/.config/fuzzel       fuzzel
-pull-config-copy $SCRIPT_PATH/../qt6ct              $HOME/.config/qt6ct        qt6ct
-pull-config-copy $SCRIPT_PATH/../kitty              $HOME/.config/kitty        kitty
-pull-config-copy $SCRIPT_PATH/../btop               $HOME/.config/btop         btop
-pull-config-copy $SCRIPT_PATH/../hypr               $HOME/.config/hypr         hyprland
-pull-config-copy $SCRIPT_PATH/../nvim               $HOME/.config/nvim         nvim
-pull-config-copy $SCRIPT_PATH/../tmux               $HOME/.config/tmux         tmux
-pull-config-copy $SCRIPT_PATH/../tmux/.tmux.conf    $HOME/.tmux.conf           tmux
-pull-config-copy $SCRIPT_PATH/../yazi               $HOME/.config/yazi         yazi
-pull-config-copy $SCRIPT_PATH/../zsh                $HOME/.config/zsh          zsh
-pull-config-copy $SCRIPT_PATH/../zsh/.zshenv        $HOME/.zshenv              zsh
-pull-config-copy $SCRIPT_PATH/../gdu/.gdu.yaml      $HOME/.gdu.yaml            gdu
-
+pull-config-copy $SCRIPT_PATH/../starship.toml      $HOME/.config/starship.toml bluetuith
+pull-config-copy $SCRIPT_PATH/../bluetuith          $HOME/.config/bluetuith     bluetuith
+pull-config-copy $SCRIPT_PATH/../fuzzel             $HOME/.config/fuzzel        fuzzel
+pull-config-copy $SCRIPT_PATH/../qt6ct              $HOME/.config/qt6ct         qt6ct
+pull-config-copy $SCRIPT_PATH/../kitty              $HOME/.config/kitty         kitty
+pull-config-copy $SCRIPT_PATH/../btop               $HOME/.config/btop          btop
+pull-config-copy $SCRIPT_PATH/../hypr               $HOME/.config/hypr          hyprland
+pull-config-copy $SCRIPT_PATH/../nvim               $HOME/.config/nvim          nvim
+pull-config-copy $SCRIPT_PATH/../tmux               $HOME/.config/tmux          tmux
+pull-config-copy $SCRIPT_PATH/../tmux/.tmux.conf    $HOME/.tmux.conf            tmux
+pull-config-copy $SCRIPT_PATH/../yazi               $HOME/.config/yazi          yazi
+pull-config-copy $SCRIPT_PATH/../zsh                $HOME/.config/zsh           zsh
+pull-config-copy $SCRIPT_PATH/../zsh/.zshenv        $HOME/.zshenv               zsh
+pull-config-copy $SCRIPT_PATH/../gdu/.gdu.yaml      $HOME/.gdu.yaml             gdu
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -9,9 +9,21 @@ source ~/.config/zsh/misc.zsh
 source ~/.config/zsh/path_update.zsh
 
 if [[ $TTY == /dev/tty1 ]]; then
-    #echo $(uwsm start Hyprland) >> /tmp/hyprland_start_tty1.txt
     echo $(start-hyprland) >> /tmp/hyprland_start_tty1.txt
     logout || exit
+elif ! [[ -z $(tty | grep pts) ]]; then
+    eval "$(starship init zsh)"
+    PROMPT_NEEDS_NEWLINE=false
+    precmd(){
+        if [[ "$PROMPT_NEEDS_NEWLINE" == true ]]; then
+            echo
+        fi
+        PROMPT_NEEDS_NEWLINE=true
+    }
+    clear(){
+        PROMPT_NEEDS_NEWLINE=false
+        command clear
+    }
 else
-    #source ~/.config/zsh/prompt.zsh
+    source ~/.config/zsh/prompt.zsh
 fi

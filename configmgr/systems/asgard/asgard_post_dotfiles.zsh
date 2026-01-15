599b5b75f5295ef8ca614df9304dcc464fba9ce4
Ayden Kranz <ayden.kranz@gmail.com>
2026-01-14 19:01:57 -0500

    Fixed some configuration stuff with yazi

--- configmgr/systems/asgard/asgard_post_dotfiles.zsh
+++ configmgr/systems/asgard/asgard_post_dotfiles.zsh
@@ -1,12 +1,12 @@
 # Add extensions to make yazi more useful
 log_message "Instsalling yazi extensions..."
+rm -r ~/.config/yazi/plugins
+rm ~/.config/yazi/package.toml
 ya pkg add yazi-rs/plugins:full-border
 ya pkg add yazi-rs/plugins:mount
-ya pkg add imsi32/yatline
-ya pkg add imsi32/yatline-catppuccin
 ya pkg add yazi-rs/plugins:smart-enter
 ya pkg add dedukun/bookmarks
 
 # Fix hyprland stuff
 sed -i 's/monitor = , 1920x1080@60, auto, 1.25/monitor = , 1920x1080@120, auto, 1/' $HOME/.config/hypr/hyprland.conf
-
+hyprctl reload

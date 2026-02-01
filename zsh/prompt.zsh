fa930e526a9f429b9a9c918a77b49340fa6f1ed1
Ayden Kranz <ayden.kranz@gmail.com>
2026-01-31 22:36:23 -0500

    ditto

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -68,7 +68,7 @@ precmd() {
     source /tmp/battery_status
 
     LEFT="%F{#cdd6f4}┌─(%F{#b4befe}$(whoami)@$(hostname))─%F{cdd6f4}[%F{#94e2d5}${PWD/#$HOME/~}%F{cdd6f4}]"
-    RIGHT="$(get_git_info) $(date +%H:%M:%S) | $symbol $percent  $time  $state"
+    RIGHT="$(get_git_info) $(date +%H:%M:%S) | $symbol $percent%%  $time  $state"
     RIGHTWIDTH=$(( $COLUMNS - $(visible_length $LEFT) ))
     print -P "$LEFT${(l:$RIGHTWIDTH:: :)RIGHT}"
 
